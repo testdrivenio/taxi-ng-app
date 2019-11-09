69b0847cdbdb5b6c4b443b5a28d5fd19f6ec25fb
Jason Parent <jason.a.parent@gmail.com>
2019-11-09 14:01:43 -0500

    Part 2 Chapter 8

--- client/src/app/components/log-in/log-in.component.spec.ts
+++ client/src/app/components/log-in/log-in.component.spec.ts
@@ -1,25 +1,49 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule, HttpTestingController
+} from '@angular/common/http/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { LogInComponent } from './log-in.component';
+import { AuthService } from '../../services/auth.service';
+import { UserFactory } from '../../testing/factories';
+import { LogInComponent } from '../log-in/log-in.component';
 
-describe('LogInComponent', () => {
+fdescribe('LogInComponent', () => {
   let component: LogInComponent;
   let fixture: ComponentFixture<LogInComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LogInComponent ]
-    })
-    .compileComponents();
-  }));
+  let router: Router;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([])
+      ],
+      declarations: [ LogInComponent ],
+      providers: [ AuthService ]
+    });
     fixture = TestBed.createComponent(LogInComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.get(Router);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should allow a user to log into an existing account', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    const user = UserFactory.create();
+    component.user = {username: user.username, password: 'pAssw0rd!'};
+    component.onSubmit();
+    const request = httpMock.expectOne('/api/log_in/');
+    request.flush(user);
+    expect(localStorage.getItem('taxi.user')).toEqual(JSON.stringify(user));
+    expect(spy).toHaveBeenCalledWith('');
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 });
--- client/src/app/components/log-in/log-in.component.ts
+++ client/src/app/components/log-in/log-in.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+class UserData {
+  constructor(
+    public username?: string,
+    public password?: string
+  ) {}
+}
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
-export class LogInComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LogInComponent {
+  user: UserData = new UserData();
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+  onSubmit(): void {
+    this.authService.logIn(
+      this.user.username, this.user.password
+    ).subscribe(user => {
+      this.router.navigateByUrl('');
+    }, (error) => {
+      console.error(error);
+    });
   }
-
 }
