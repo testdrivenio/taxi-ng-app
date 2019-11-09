1a3576428672be9738ed630f77579ca9f473d38f
Jason Parent <jason.a.parent@gmail.com>
2019-11-09 13:38:18 -0500

    Part 2 Chapter 3

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LogInComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'sign-up', component: SignUpComponent },
+      { path: 'log-in', component: LogInComponent },
+      { path: '', component: LandingComponent }
+    ], { useHash: true })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
