1ba0a43fe358906628bb5d450d466cc3e3bfc0a0
Jason Parent <jason.a.parent@gmail.com>
2020-08-22 22:09:21 -0400

    Remove deleted files

--- client/src/app/components/driver/driver.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-driver',
-  templateUrl: './driver.component.html',
-  styleUrls: ['./driver.component.css']
-})
-export class DriverComponent {}
--- client/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  GOOGLE_API_KEY: 'AIzaSyATr-rYgRvYr5-RVN5JIJlOounM86arm6I'
-};
